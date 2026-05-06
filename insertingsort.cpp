3198aa712062021d873209fb8999bc4ca8862235
Vansh Nain <vanshnainhere@gmail.com>
2026-05-07 00:19:49 +0530

    Update insertingsort.cpp

--- insertingsort.cpp
+++ insertingsort.cpp
@@ -6,6 +6,7 @@ int insertionSort(vector<int>& arr) {
     for (int i = 1; i < arr.size(); i++) {
         int key = arr[i];
         int j = i - 1;
+
         while (j >= 0) {
             comparisons++;
             if (arr[j] > key) {
@@ -18,10 +19,27 @@ int insertionSort(vector<int>& arr) {
     return comparisons;
 }
 int main() {
-    int n; cin >> n;
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
     vector<int> arr(n);
-    for(int i=0;i<n;i++) cin>>arr[i];
+    cout << "Enter elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
     int c = insertionSort(arr);
-    for(int x:arr) cout<<x<<" ";
-    cout<<"\nComparisons: "<<c;
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\nComparisons: " << c << endl;
+    return 0;
 }
+
+//input
+//and Enter number of elements: 5
+//Enter elements: 5 2 4 6 1
+//    output
+//    Sorted array: 1 2 4 5 6
+//Comparisons: 8
+    
